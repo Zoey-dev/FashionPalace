c59b49c7a47c3f53b4cf13fb81693b623a236e64
Catalina <catalina@Catalinas-MacBook-Pro-2.local>
2020-10-05 22:39:03 +0100

    Build Django Project and  Applications

---
+++ clothings/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from . models import Clothing
+
+# Register your models here.
+
+
+admin.site.register(Clothing)
---
+++ clothings/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+
+from django.http import HttpResponse
+
+from .models import Clothing
+# Create your views here.
+def index(request):
+    clothings = Clothing.objects
+    return render(request, 'clothings/index.html', {'clothings': clothings})
+
+
+def detail(request, clothing_id):
+    clothing_detail = get_object_or_404(Clothing, pk=clothing_id)
+
+    return render(request, 'clothings/detail.html', {'clothing': clothing_detail})
